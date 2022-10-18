8ce36e494cc53f599ada6ce820f5a02ac28cdb0c
leelingzhen <leeling.zhenn+github@gmail.com>
2022-10-18 13:10:12 +0800

    first commit

---
+++ a470r_path_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/leelingzhen/me3243_real_ros_labs/a470r_path_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
